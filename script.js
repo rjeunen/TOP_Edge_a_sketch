5fee4a242d29987f6cd300be53d3dbc79b487766
Robby Jeunen <robbyjeunen@gmail.com>
2024-03-25 20:57:06 +0100

    erase and color buttons added - js code added

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ let defaultGridSize = 16;
 let gridSize = defaultGridSize * defaultGridSize;
 let defaultFlexItem = (100 / defaultGridSize) + "%";
 
-console.log(typeof flexItem);
-
+let gridItem = document.querySelectorAll("gridItem");
 let gameBoard = document.querySelector("#gameBoard");
+let eraseButton = document.querySelector("#eraseButton");
+let colorButton = document.querySelector("#colorButton");
 
 //creating div's
 function createDiv(){
     let div = document.createElement("div");
     div.style.border = "1px solid red";
+    div.className = "gridItem";
     div.style.width = "100vw";
     div.style.flex = defaultFlexItem; //bepaald percentage van een div
     gameBoard.appendChild(div);
@@ -19,3 +21,18 @@ for(let i = 0; i < gridSize; i++){
     createDiv();
 }
 
+gameBoard.addEventListener("mouseover", (event) => {
+    event.target.style.backgroundColor = "blue";
+});
+
+eraseButton.addEventListener("click", () => {
+    gameBoard.addEventListener("mouseover", (event) =>{
+        event.target.style.backgroundColor = "";
+    })
+})
+
+colorButton.addEventListener("click", () => {
+    gameBoard.addEventListener("mouseover", (event) =>{
+        event.target.style.backgroundColor = "blue";
+    })
+})
