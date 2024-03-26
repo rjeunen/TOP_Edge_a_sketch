128ec84fa4aa302591564cce9d5800b32feab5b8
Robby Jeunen <robbyjeunen@gmail.com>
2024-03-26 10:33:56 +0100

    added functions create and delete grid. Added listener van reset button

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ let defaultGridSize = 16;
 let gridSize = defaultGridSize * defaultGridSize;
 let defaultFlexItem = (100 / defaultGridSize) + "%";
 
-let gridItem = document.querySelectorAll("gridItem");
+let gridItem = document.querySelectorAll("div.gridItem");
 let gameBoard = document.querySelector("#gameBoard");
 let eraseButton = document.querySelector("#eraseButton");
 let colorButton = document.querySelector("#colorButton");
+let gridReset = document.querySelector("#gridReset");
 
 //creating div's
 function createDiv(){
@@ -17,9 +18,25 @@ function createDiv(){
     gameBoard.appendChild(div);
 }
 
-for(let i = 0; i < gridSize; i++){
-    createDiv();
+//resetten existing div's - remove background color
+//WERKT NOG NIET
+function resetGrid () {
+    gridItem.style.backgroundColor = "";
 }
+
+function deleteGrid(){
+    while (gameBoard.firstChild){
+        gameBoard.removeChild(gameBoard.firstChild);
+    }
+}
+
+function createGrid(){
+    for(let i = 0; i < gridSize; i++){
+        createDiv();
+    }
+}
+
+
 
 gameBoard.addEventListener("mouseover", (event) => {
     event.target.style.backgroundColor = "blue";
@@ -37,11 +54,21 @@ colorButton.addEventListener("click", () => {
     })
 })
 
+//WERKT NOG NIET
+gridReset.addEventListener("click", () => {
+    deleteGrid();
+    createGrid();
+})
+
+createGrid();
+
 /*
 next steps:
 1. add function behind reset grid button -- clear all divs of color.
-    -- functie + eventlistener
+    -- functie + eventlistener -- propertie 'not defined' ...
 2. add function behind new grid size button
+    -- DELETE function
+        -- remove child met classname.
     -- probably rework needed after this button.
         -- in mijn create zit defaultFlexItem an deze kijkt naar de 16
         -- kan ik die default aanpassen?
